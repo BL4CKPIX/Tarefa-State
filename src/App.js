f31d12481ddf4bfbbdfa24fc08fd8abbefd12bae
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-07-10 20:43:51 -0300

    complet state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class app extends Component{
+      state = {
+        nome: '----JØRDAN',
+        idade: '----19',
+        comidaFavorita: '----Pizza',
+        musicasFavoritas: ['Hadaka no Yuusha','Imagine Dragons - Enemy','Ana Vilela - Trem-Bala','Melim - Meu Abrigo']
+      }
+      render (){
+        return(
+          <div>
+            <h1>{this.state.nome}</h1>
+            <h2>{this.state.idade}</h2>
+            <h3>{this.state.comidaFavorita}</h3>
+            <nav>
+              <ul>
+                <li>{this.state.musicasFavoritas[0]}</li>
+                <li>{this.state.musicasFavoritas[1]}</li>
+                <li>{this.state.musicasFavoritas[2]}</li>
+                <li>{this.state.musicasFavoritas[3]}</li>
+              </ul>
+            </nav>
+          </div>
+        )
+      }
 }
-
-export default App;
+export default app;
